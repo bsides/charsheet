ccd62edd83255068e539f18f2c2e272ae6e29544
Rafael Pereira <rafaelbsides@gmail.com>
2014-05-19 17:17:00 -0300

    Changes in spec and new html for tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+(function (window) {
+  'use strict';
+
+  var CharSheet = {
+    init: function() {
+      if (localStorage['name']) {
+        $('#name').val(localStorage["name"]);
+      }
+      if (localStorage['player']) {
+        $('#player').val(localStorage["player"]);
+      }
+    }() // execute this on page load!
+  };
+
+  window.CharSheet = CharSheet;
+}(this));
+
+$('.form-control').keyup(function () {
+  localStorage[$(this).attr('name')] = $(this).val();
+});
+
+$('#localStorageTest').submit(function() {
+  localStorage.clear();
+});
